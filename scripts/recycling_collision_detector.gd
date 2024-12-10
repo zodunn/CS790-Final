7876f9c8db87c2d9d84c7df8fb19606a9041750a
zodunn <90057894+zodunn@users.noreply.github.com>
2024-12-10 11:11:55 -0600

    added trash scenes

--- scripts/recycling_collision_detector.gd
+++ scripts/recycling_collision_detector.gd
@@ -1,6 +1,9 @@
 extends RigidBody3D
 
-@export var recycling_type: String = 'paper'
+var speed: float = 10.0
+var target: Node3D
+signal remove
+var clean_state: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,16 +12,27 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if target:
+		var direction = (target.global_transform.origin - global_transform.origin).normalized()
+		global_transform.origin += direction * speed * delta
+		
+		if global_transform.origin.distance_to(target.global_transform.origin) < 0.1:
+			queue_free()
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	var obj_collided_with = area.get_parent_node_3d()
 	if obj_collided_with.name == 'washbin':
 		# add code for setting clean state
-		pass
-	elif obj_collided_with.name == recycling_type:
+		clean_state = true
+	elif obj_collided_with.name == name and clean_state:
 		# add code for collecting points
-		queue_free()
+		delete()
 	else:
 		pass
+
+
+func delete():
+	# Emit the signal before freeing the object
+	remove.emit(self)
+	self.queue_free()
